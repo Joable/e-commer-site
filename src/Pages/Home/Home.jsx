46340c3fbed24b26953b1d6ef5c7c79d5f5b899a
Joaquin <jaltable@fi.uba.ar>
2023-06-28 19:46:22 -0300

    created 'Home' page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+    return ( 
+        <>
+        <header>
+            <h1>
+                Header content
+            </h1>
+        </header>
+
+        <body>
+            <h2>Body content</h2>
+        </body>
+
+        <footer>
+            <h2>
+                Footer content
+            </h2>
+        </footer>
+        </>
+    );
+}
+
+export default Home;
