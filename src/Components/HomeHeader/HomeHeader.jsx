5d0c82f29a1e95d224b549ed0b4102d9623e3f66
Joaquin <jaltable@fi.uba.ar>
2023-06-29 21:46:26 -0300

    added text and lower brightness to 'HomeHeader'

--- src/Components/HomeHeader/HomeHeader.jsx
+++ src/Components/HomeHeader/HomeHeader.jsx
@@ -12,22 +12,30 @@ function HomeHeader() {
             <div className={styles.main}>
                 <a href="#">
                     <img src={furniture} alt="Furniture"/>
+                    <p></p>
+                    <h3>Furniture</h3>
                 </a>
             </div>
 
             <div className={styles.second}>
                 <a href="#">
                     <img src={clothes} alt="Clothes" />
+                    <p></p>
+                    <h3>Clothes</h3>
                 </a>
             </div>
 
             <div className={styles.third}>
                 <a href="#">
                     <img src={peripherals} alt="Peripherals" />
+                    <p></p>
+                    <h3>Peripherals</h3>
                 </a>
 
                 <a href="#">
                     <img src={electronics} alt="Electronics" />
+                    <p></p>
+                    <h3>Electronics</h3>
                 </a>
             </div>
         </div>
