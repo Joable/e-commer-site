3109c9eaa051a7e414b66b23678e16986be710a7
Joaquin <jaltable@fi.uba.ar>
2023-06-30 17:17:58 -0300

    created 'ProductItem' component

---
+++ src/Components/ProductItem/ProductIem.jsx
@@ -0,0 +1,7 @@
+function ProductItem() {
+    return (  
+        <h3>Product</h3>
+    );
+}
+
+export default ProductItem;
--- src/Components/TrendItems/TrendItems.jsx
+++ src/Components/TrendItems/TrendItems.jsx
@@ -1,6 +1,18 @@
+import styles from './TrendItems.module.css'
+
 function TrendItems() {
+    const arrow1 = "<--", arrow2 = "-->"
+    
     return ( 
-         <h3>Trending Products</h3>
+        <div className={styles.trendHeader}>
+            <h3>Trending Products</h3>
+
+            <div>
+                <button>{arrow1}</button>
+                <button>{arrow2}</button>
+            </div>
+        </div>
+        
     );
 }
 
