244b553212afd4e54283c601f249e03acfba5ea8
Joaquin <jaltable@fi.uba.ar>
2023-07-05 19:34:36 -0300

    added buttons and a logo to 'Footer'

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,23 @@
+import styles from './Footer.module.css';
+
 function Footer() {
     return ( 
-        <h1>Footer</h1>
+        <footer className={styles.footer}>
+            <div className={styles.footerBody}>
+                <a href="#">About</a>
+
+                <a href="#">FAQs</a>
+
+                <a href="#">Contact Us</a>
+
+                <a href="#">News</a>
+            </div>
+            <div className={styles.footerLogo}>
+                <h2>
+                    E-Comerce Logo                    
+                </h2>
+            </div>
+        </footer>
     );
 }
 
