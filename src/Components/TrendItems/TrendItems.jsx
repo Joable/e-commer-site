23ef8451dfd8e696631587603e366455ab804bb6
Joaquin <jaltable@fi.uba.ar>
2023-07-04 22:22:13 -0300

    added a carousel functionallity to 'TrendItems', maybe make a component out of it

--- src/Components/TrendItems/TrendItems.jsx
+++ src/Components/TrendItems/TrendItems.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import ProductItem from '../ProductItem/ProductIem';
 import styles from './TrendItems.module.css'
 
 function TrendItems() {
+    const htmlElement = document.getElementById('trendItems');
     const arrow1 = "<--", arrow2 = "-->";
-    const iterator = [0,0,0,0,0];
+    const iterator = [0,0,0,0,0,0,0,0,0,0];
+
+    useEffect(() => {
+        const xAxisValue = getComputedStyle(document.getElementById('trendItems')).getPropertyValue('--x-axis')
+        console.log(xAxisValue)
+    },[]);
+
+    const moveXAxis = (value) => document.getElementById('trendItems').style.setProperty('--x-axis', value);
     
     return ( 
         <>
@@ -11,14 +20,16 @@ function TrendItems() {
             <h2>Trending Products</h2>
 
             <div>
-                <button>{arrow1}</button>
-                <button>{arrow2}</button>
+                <button onClick={() => moveXAxis("0%")}>{arrow1}</button>
+                <button onClick={() => moveXAxis("-50%")}>{arrow2}</button>
             </div>
 
         </div>
 
         <div className={styles.trendBody}>
-            {iterator.map(()=><ProductItem/>)}
+            <div id='trendItems' className={styles.trendItems}>
+                {iterator.map(()=><ProductItem/>)}
+            </div>
         </div>
         
         </>
