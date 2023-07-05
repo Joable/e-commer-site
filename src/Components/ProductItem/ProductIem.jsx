15c112b16128054f9f012791cb8af0c44db696fe
Joaquin <jaltable@fi.uba.ar>
2023-07-05 18:29:04 -0300

    now 'Separator' has text and an image

--- src/Components/ProductItem/ProductIem.jsx
+++ src/Components/ProductItem/ProductIem.jsx
@@ -5,7 +5,7 @@ import placholder from '../../img/osaka2.PNG';
 function ProductItem() {
     return (  
         <div className={styles.item}>
-            <img src={placholder} alt="Product image"/>
+            <img src={placholder} alt="Product"/>
 
             <div className={styles.itemBody}>
                 <h4>Product name</h4>
--- src/Components/Separator/Separator.jsx
+++ src/Components/Separator/Separator.jsx
@@ -5,9 +5,22 @@ function Separator() {
     return ( 
         <>
         <div className={styles.separatorBody}>
-            <div className={styles.text}>Text</div>
 
-            <img src={image} alt="Separator image"/>
+            <div className={styles.textContainer}>
+                <div className={styles.textBody}>
+                    <h2>Title</h2>
+
+                    <p>
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Sed eleifend leo pellentesque, pharetra ex eget, tempor risus.
+                    </p>
+
+                    <button className={styles.separatorButton}>SHOP NOW</button>
+                </div>
+            </div>
+
+            <img src={image} alt="Separator"/>
+
         </div>
         </>
     );
