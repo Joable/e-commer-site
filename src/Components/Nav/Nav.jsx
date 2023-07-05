4bc67d24911360f3966990d10b98bd00f3edc52b
Joaquin <jaltable@fi.uba.ar>
2023-07-05 20:21:06 -0300

    created 'Nav' component

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import styles from './Nav.module.css';
+
+function Nav() {
+    return ( 
+        <>
+        <nav>
+            <h1>NAV</h1>
+        </nav>
+        </>
+     );
+}
+
+export default Nav;
