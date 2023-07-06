acb5021afaf7fd0b105f6fcd62a4d2ece53134eb
Joaquin <jaltable@fi.uba.ar>
2023-07-06 17:59:34 -0300

    added routing to 'App'

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
 import Footer from './Components/Footer/Footer';
 import Nav from './Components/Nav/Nav';
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <>
+    <Router>
     <Nav/>
 
     <div className="center">
-      <Home/>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
 
     <Footer/>
-    </>
+    </Router>
   );
 }
 
