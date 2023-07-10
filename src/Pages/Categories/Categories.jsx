48801966f86f1e03964334a6b306cdb497104c52
Joaquin <jaltable@fi.uba.ar>
2023-07-10 14:13:40 -0300

    added buttons to 'Categories'

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,6 +1,30 @@
+import styles from './Categories.module.css'
+
 function Categories() {
-    return ( 
-        <h1>Categories</h1>
+    return (
+        <>
+        <div className={styles.categoriesBody}>
+            <div className={styles.categoriesTitle}>
+                <h1>Categories</h1>
+            </div>
+
+            <div className={styles.categoriesButtons}>
+                <button>All</button>
+                
+                <button>Furnitures</button>
+                
+                <button>Kitchen</button>
+                
+                <button>Lamps</button>
+                
+                <button>Clothes</button>
+                
+                <button>Electronics</button>
+                
+                <button>Peripherals</button>
+            </div>
+        </div>
+        </> 
     );
 }
 
