1721110fd2bed268827c558fc77386080abcd3eb
Joaquin <jaltable@fi.uba.ar>
2023-07-10 20:37:33 -0300

    added new style classes to 'Home'

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import styles from './Home.module.css';
+
 import HomeHeader from "../../Components/HomeHeader/HomeHeader";
 import Separator from "../../Components/Separator/Separator";
 import TrendItems from "../../Components/TrendItems/TrendItems";
@@ -5,15 +7,19 @@ import TrendItems from "../../Components/TrendItems/TrendItems";
 function Home() {
     return ( 
         <>
-        <div>
+        <div className={styles.homeHeader}>
             <HomeHeader/>
         </div>
 
-        <div>
+        <div className={styles.separator}>
             <Separator/>
-
+        </div>
+        
+        <div className={styles.trendItems}>
             <TrendItems/>
-            
+        </div>
+        
+        <div className={styles.separator}>
             <Separator/>
         </div>
         </>
