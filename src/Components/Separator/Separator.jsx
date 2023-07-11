78d97419f47345af592e5acaabc2f53f88826fd2
Joaquin <jaltable@fi.uba.ar>
2023-07-11 17:13:57 -0300

    changed styles for the button

--- src/Components/Separator/Separator.jsx
+++ src/Components/Separator/Separator.jsx
@@ -1,6 +1,6 @@
-import styles from './Separator.module.css';
 import image from '../../img/osaka2.PNG';
-import { useEffect } from 'react';
+import styles from './Separator.module.css';
+
 
 function Separator({parameters}) {
 
