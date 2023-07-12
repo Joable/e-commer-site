21f5fedac7a7bc36aeb83bfc428668fe62819aca
Joaquin <jaltable@fi.uba.ar>
2023-07-12 13:09:10 -0300

    added firebase configuration

---
+++ src/Config/Firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPGAs0eYenq12PxkWePDoQNBY8mSeONCM",
+    authDomain: "e-commerce-fcdf9.firebaseapp.com",
+    projectId: "e-commerce-fcdf9",
+    storageBucket: "e-commerce-fcdf9.appspot.com",
+    messagingSenderId: "819949723338",
+    appId: "1:819949723338:web:fd9beb6873531cf6fd9e33"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.db = firebase.firestore();
+
+  export default firebase
