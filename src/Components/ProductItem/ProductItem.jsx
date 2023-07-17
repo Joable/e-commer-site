6c11f29d6b13f1584e23e41b1bbc0c80e88e9085
Joaquin <jaltable@fi.uba.ar>
2023-07-17 18:11:00 -0300

    changed some styles from 'ProductItem', 'ProductCarousel' and 'App' to better image visibility

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -6,13 +6,15 @@ function ProductItem({id, productData}) {
 
     return (  
         <Link className={styles.item} to={`/product/${id}`} target="_blank">
-                    <img src={productData.images[0]} alt="Product"/>
+            <div className={styles.itemImage}>
+                <img src={productData.images[0]} alt="Product"/>
+            </div>
 
-                    <div className={styles.itemBody}>
-                        <h4>{productData.name}</h4>
+            <div className={styles.itemBody}>
+                <h4>{productData.name}</h4>
 
-                        <h3>{productData.price}$</h3>
-                    </div>
+                <h3>{productData.price}$</h3>
+            </div>
        </Link>
 
     );
