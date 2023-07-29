0833a9d73bf5d68cc5f3f982890c0e3711f421f4
Joaquin <jaltable@fi.uba.ar>
2023-07-29 12:13:31 -0300

    'ProductQuantity' has functionallity

--- src/Components/ProductQuantity/ProductQuantity.jsx
+++ src/Components/ProductQuantity/ProductQuantity.jsx
@@ -3,17 +3,24 @@ import styles from './ProductQuantity.module.css';
 import { useState } from 'react';
 
 export default function ProductQuantity(){
-    const [quantity, setQuantity] = useState(0)
+    const [quantity, setQuantity] = useState(0);
+
+    const add = () => {
+        setQuantity(quantity + 1);
+    };
+    const substract = () => {
+        if(quantity > 0)setQuantity(quantity - 1);
+    };
     
     return(
         <div className={styles.count}>
-            <button>-</button>
+            <button onClick={substract}>-</button>
 
             <div className={styles.countDisplay}>
                 <h3>{quantity}</h3>
             </div>
 
-            <button>+</button>
+            <button onClick={add}>+</button>
         </div>
     );
 }
