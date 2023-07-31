691379c361349a0969c19fb5e9bd622a12c77dd9
Joaquin <jaltable@fi.uba.ar>
2023-07-31 19:16:24 -0300

    'Subtotal' tracks the prices and quantities of the cart products

--- src/Components/Subtotal/Subtotal.jsx
+++ src/Components/Subtotal/Subtotal.jsx
@@ -1,15 +1,31 @@
 import styles from './Subtotal.module.css';
 
-import { useState } from 'react';
+import { 
+    useState,
+    useContext,
+    useEffect
+} from 'react';
+
+import { CartContext } from '../../Context/CartContext';
 
 export default function Subtotal(){
-    const [subtotal, setSubtotal] = useState(0);
+    const {cartProducts, setCartProducts} = useContext(CartContext);
+
+    const calculateSubtotal = () => {
+        let subtotal = 0;
+
+        Object.keys(cartProducts).forEach((id) => {
+            subtotal = subtotal + (cartProducts[id].quantity * cartProducts[id].price);
+        });
+
+        return subtotal
+    }
 
     return(
         <div className={styles.subtotal}>
             <h3>Subtotal</h3>
 
-            <h4>{subtotal}$</h4>
+            <h4>{calculateSubtotal()}$</h4>
         </div>
     );
 }
