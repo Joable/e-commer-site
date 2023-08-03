893ba29e9d7177edf9da7f94a224814d175824bf
Joaquin <jaltable@fi.uba.ar>
2023-08-03 19:56:10 -0300

    'CartItem' name now redirects to the product page

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -1,28 +1,32 @@
 import styles from './CartItem.module.css';
 
-import { useState } from 'react';
-
 import CartQuantity from '../CartQuantity/CartQuantity';
 import CartDelete from '../CartDelete/CartDelete';
 
 export default function CartItem({id, product}){
     return(
         <div className={styles.item}>
-        <img src={product.image} alt={product.name} />
 
-        <div className={styles.itemDetails}>
-            <div className={styles.detailsHeader}>
-                <h4>{product.name}</h4>
-                    
-                <h3>{product.price}$</h3>
+            <img src={product.image} alt={product.name} />
+
+            <div className={styles.itemDetails}>
+
+                <div className={styles.detailsHeader}>
+                    <a href={`/product/${id}`}>
+                        <h3>{product.name}</h3>
+                    </a>
+                            
+                        <h3><b>{product.price}</b>$</h3>
+                </div>
+
+                <div className={styles.detailsQuantity}>
+                    <CartQuantity id={id}/>
+
+                    <CartDelete id={id}/>
+                </div>
+
             </div>
 
-            <div className={styles.detailsQuantity}>
-                <CartQuantity id={id}/>
-
-                <CartDelete id={id}/>
-            </div>
         </div>
-    </div>
     );
 }
