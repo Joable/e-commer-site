dc12cb9016232fe36ff8b3db8beb6b3c95d4bedd
Joaquin <jaltable@fi.uba.ar>
2023-09-06 16:45:26 -0300

    added 'paginate' function to 'Pagination'

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -7,7 +7,7 @@ import PageNumbers from "../PageNumbers/PageNumbers";
 
 export default function Pagination({products}){
     const [currentPage, setCurrentPage] = useState(1);
-    const [productsPerPage, setProductsPerPage] = useState(4);
+    const [productsPerPage] = useState(8);
 
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
@@ -15,14 +15,15 @@ export default function Pagination({products}){
 
     const displayProducts = (productsToDiplay) => {
         return productsToDiplay.map((product) => <ProductItem id={product.id} productData={product.data()}/>)
+    };
 
-    }
+    const paginate = (pageNumber) => setCurrentPage(pageNumber); 
 
     return(
         <>
         {displayProducts(currentProducts)}
 
-        <PageNumbers totalProducts={products.length} productsPerPage={productsPerPage}/>
+        <PageNumbers totalProducts={products.length} productsPerPage={productsPerPage} paginate={paginate}/>
         </>
     );
 }
