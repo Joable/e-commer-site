6f7d8230fb6c32f0ce61f69661c88b8e77830cd8
Joaquin <jaltable@fi.uba.ar>
2023-09-11 00:28:10 -0300

    created 'Spinner' component

--- src/Components/ProductItem/LoadingProductItem.jsx
+++ src/Components/ProductItem/LoadingProductItem.jsx
@@ -2,6 +2,15 @@ import styles from './LoadingProductItem.module.css';
 
 export default function LoadingProductItem(){
     return(
+        <div className={styles.item}>
 
+            <div className={styles.itemImage}>
+                
+            </div>
+
+            <div className={styles.itemBody}>
+                <h4>Loading..</h4>
+            </div>
+        </div>
     );
-}
+};
