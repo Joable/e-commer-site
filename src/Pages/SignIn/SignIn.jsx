882f8ca6416c85259d706ecccef8c22784440718
Joaquin <jaltable@fi.uba.ar>
2023-11-24 19:19:51 -0300

    added functionallity to the 'SignIn' form

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,40 +1,67 @@
 import styles from './SignIn.module.css';
 
+import { useState } from 'react';
+
 function SignIn(){
+    const [form, setForm] = useState({
+        username:"",
+        email:"",
+        password:"",
+        confirmEmail:"",
+        confirmPassword:""
+    });
+
+    const handleChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        setForm({
+            ...form,
+            [name] : value
+        });
+    };
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        console.log(form);
+    };
+
     return(
         <>
-            <form action="">
+            <form onSubmit={handleSubmit} autoComplete='off' className={styles.signin}>
                 <label>
                     Username:
                     
-                    <input type="text" />
+                    <input name='username' value={form.username} type="text" onChange={handleChange} />
                 </label>
 
                 <label>
                     Email:
                     
-                    <input type="email" />
+                    <input name='email' value={form.email} type="email" onChange={handleChange} />
                 </label>
 
                 <label>
                     Confirm Email:
                 
-                    <input type="email" />
+                    <input name='confirmEmail' value={form.confirmEmail} type="email" onChange={handleChange} />
                 </label>
 
                 <label>
                     Password:
                 
-                    <input type="password" />
+                    <input name='password' value={form.password} type="password" onChange={handleChange} />
                 </label>
 
                 <label>
                     Confirm Password:
                     
-                    <input type="password" />
+                    <input name='confirmPassword' value={form.confirmPassword} type="password" onChange={handleChange} />
                 </label>
 
-                <button>
+                <button type='submit'>
                     Create Account
                 </button>
             </form>
